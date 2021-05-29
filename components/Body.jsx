fee4de5cff4eae5a29a33376094b2f9121d9fa52
David Manor <david@dolphextechnologies.com>
2021-05-29 16:22:43 +0100

    add hero description

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,34 +1,37 @@
-import React from 'react'
-import styles from '../styles/body.module.css'
+import React from "react";
+import styles from "../styles/body.module.css";
 
 const Body = () => {
-    return (
-        <div className={styles.bodyContainer}>
-            <span className={styles.bannerContainer}>
-            <div className={styles.releaseBanner}></div>
-            <div className={styles.bannerContent}>
-            <img
-              src="/release-logo.png"
-              alt="release"
-              className={styles.releaseLogo}
-            />
-            <div className={styles.releaseNameWrapper}>
-              <span className={styles.releaseName}>
-                Linear Release 2020.12 + Series A
-              </span>
-            </div>
-            <span className={styles.hBar}>|</span>
-            <span className={styles.whatsNew}>See what’s new →</span>
+  return (
+    <div className={styles.bodyContainer}>
+      <span className={styles.bannerContainer}>
+        <div className={styles.releaseBanner}></div>
+        <div className={styles.bannerContent}>
+          <img
+            src="/release-logo.png"
+            alt="release"
+            className={styles.releaseLogo}
+          />
+          <div className={styles.releaseNameWrapper}>
+            <span className={styles.releaseName}>
+              Linear Release 2020.12 + Series A
+            </span>
           </div>
-            </span>
-            <span className={styles.titleWrapper}>
-              <h1 className={styles.heroTitle}>
-              The issue tracking <br /> tool you'll enjoy using
-              </h1>
-            </span>
-
+          <span className={styles.hBar}>|</span>
+          <span className={styles.whatsNew}>See what’s new →</span>
         </div>
-    )
-}
+      </span>
+      <span className={styles.titleWrapper}>
+        <h1 className={styles.heroTitle}>
+          The issue tracking <br /> tool you'll enjoy using
+        </h1>
+        <p className={styles.heroDescription}>
+          Linear helps streamline software projects, sprints, tasks, and bug
+          tracking. It's built for high-performance teams.
+        </p>
+      </span>
+    </div>
+  );
+};
 
-export default Body
+export default Body;
