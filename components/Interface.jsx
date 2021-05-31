5f875be363b2891d8e3f27083f60038dd9b2e9b9
David Manor <david@dolphextechnologies.com>
2021-05-31 11:06:05 +0100

    add interface component

---
+++ components/Interface.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "../styles/interface.module.css";
+import Image from "next/image";
+
+const Interface = () => {
+  return (
+    <div className={styles.interfaceContainer}>
+      <div className={styles.interfaceWrapper}>
+        <div className={styles.interfaceContent}>
+          <div className={styles.descriptionContainer}>
+            <div className={styles.description}>
+              <h2 className={styles.heading}>Interface, streamlined.</h2>
+              <div className={styles.descriptionContent}>
+                <p className={styles.descriptionWriteup}>
+                  <b className={styles.bold}>Robust.</b>
+                  {
+                    " Fast to navigate. Create issues in seconds. Add priorities, labels and estimates."
+                  }
+                </p>
+                <p className={styles.descriptionWriteup}>
+                  <b className={styles.bold}> List and board.</b>
+                  {"  See your issues in either a list or board view."}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.screenshotWrapper}>
+            <div className={styles.screenshotcontainer}>
+              <div className={styles.gradientBg}></div>
+              <img
+                src="/interface2.png"
+                alt="gradientbg2"
+                srcset=""
+                className={styles.screenshotTwo}
+              />
+              <img
+                src="/interface1.png"
+                alt="gradientbg1"
+                srcset=""
+                className={styles.screenshotOne}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Interface;
