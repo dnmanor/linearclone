f19d18a4ca7d7238f2d8e007e74e81e150d973ff
David Manor <david@dolphextechnologies.com>
2021-05-31 06:51:09 +0100

    add features data

---
+++ data/features.js
@@ -0,0 +1,27 @@
+export const features = [
+  {
+    name: "Built for speed",
+    description:
+      "Synchronized in real-time across all users. No spinners or waiting.",
+  },
+  {
+    name: "Keyboard first design",
+    description: "Optimized for efficiency with extensive keyboard shortcuts.",
+  },
+  {
+    name: "For software teams",
+    description: "Created by software people for software product teams.",
+  },
+  {
+    name: "Works offline",
+    description: "Access and make changes with or without internet access.",
+  },
+  {
+    name: "Light and dark UI",
+    description: "We have multiple themes. Use light or dark, your choice.",
+  },
+  {
+    name: "Multiple teams",
+    description: "Have all your teams in one shared workspace.",
+  },
+];
