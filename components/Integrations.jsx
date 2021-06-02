f798d3f9ae0ab38fed55fdabf8960f46e9e8e338
David Manor <manornuertey@gmail.com>
2021-06-02 10:28:19 +0000

    add buttom section of integration

--- components/Integrations.jsx
+++ components/Integrations.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import styles from "../styles/integrations.module.css";
-import { Github, Gitlab, Sentry, Figma, Slack } from "../components/icons";
+import {
+  Github,
+  Gitlab,
+  Sentry,
+  Figma,
+  Slack,
+  Zapier,
+  API,
+} from "../components/icons";
 
 const Integrations = () => {
   return (
@@ -45,11 +53,7 @@ const Integrations = () => {
           </div>
         </div>
         <div className={styles.screenshotContainer}>
-          <img
-            src="/figma.png"
-            alt="figma"
-            className={styles.screenshot}
-          />
+          <img src="/figma.png" alt="figma" className={styles.screenshot} />
         </div>
       </div>
       <div className={styles.automatedTracking}>
@@ -63,13 +67,36 @@ const Integrations = () => {
           </div>
         </div>
         <div className={styles.screenshotContainer}>
-          <img
-            src="/slack2.png"
-            alt="slack"
-            className={styles.screenshot}
-          />
+          <img src="/slack2.png" alt="slack" className={styles.screenshot} />
         </div>
       </div>
+      <div className={styles.smallerContainer}>
+        <div className={styles.smallIntegration}>
+          <Zapier />
+          <div className={styles.smallDescription}>
+            <h4 className={styles.smallTitle}>
+              Connect with 1000+ tools using Zapier
+            </h4>
+            <p className={styles.p}>
+              Create new issues based on triggers from Zapier applications.
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.smallIntegration}>
+          <API />
+          <div className={styles.smallDescription}>
+            <h4 className={styles.smallTitle}>
+              Custom workflows using our API
+            </h4>
+            <p className={styles.p}>
+              {
+                "Query and mutate data using our GraphQL API to build custom workflows."
+              }
+            </p>
+          </div>
+        </div>
+      </div>ß
     </div>
   );
 };
